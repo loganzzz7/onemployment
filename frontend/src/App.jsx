b3f36da8ca22fd5cd9bd98c5efcf732454c2a574
loganzzz7 <112505545+loganzzz7@users.noreply.github.com>
2025-06-27 00:09:26 -0500

    27th

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import { Button, HStack } from "@chakra-ui/react"
+import { Box, Button, HStack } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+
+import LandingPage from "./pages/LandingPage"
+import ConnectPage from "./pages/ConnectPage"
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
-    <>
-      <HStack>
-        <Button>test</Button>
-      </HStack>
-    </>
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/connect" element={<ConnectPage />} />
+
+      </Routes>
+    </Box>
   )
 }
 
