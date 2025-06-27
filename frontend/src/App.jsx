71e814e8d131ff9d44ad49b8a247785295bb9b31
loganzzz7 <112505545+loganzzz7@users.noreply.github.com>
2025-06-26 23:30:51 -0500

    chakra ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button, HStack } from "@chakra-ui/react"
+
+function App() {
+
+  return (
+    <>
+      <HStack>
+        <Button>test</Button>
+      </HStack>
+    </>
+  )
+}
+
+export default App
